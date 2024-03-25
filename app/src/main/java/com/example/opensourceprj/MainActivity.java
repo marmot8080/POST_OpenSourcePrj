9aee657a6c120941adf7332cdc297df7480d63d2
marmot8080 <marmot8080@gmail.com>
2024-03-25 22:22:48 +0900

    merged ble.java into MainActivity.java

--- app/src/main/java/com/example/opensourceprj/MainActivity.java
+++ app/src/main/java/com/example/opensourceprj/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.opensourceprj;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -13,6 +17,9 @@ import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,11 +32,12 @@ public class MainActivity extends AppCompatActivity {
     private Retrofit retrofit;
     private comm_data service;
     private BluetoothAdapter blead;
-    private CustomDialog customDialog;
 
     private Button btn_test;
 
-    public static final String user = "POST";
+    private static final String raspberryPiAddr = "B8:27:EB:7F:E7:58";
+    private String OTP_data = null;
+    private static final String user = "2jo, minwoo, taeho, hyungwoo, bogu, wuyixin";
     private static String data = null;
 
     @Override
@@ -38,20 +46,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 객체 생성
-        BluetoothAdapter blead = BluetoothAdapter.getDefaultAdapter();
+        blead = BluetoothAdapter.getDefaultAdapter();
 
-        if(blead == null) {
+        if (blead == null) {
             Toast.makeText(getApplicationContext(), "Bluetooth is not available", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "Bluetooth is available", Toast.LENGTH_SHORT).show();
         }
-
-        if(!blead.isEnabled()) {
+        
+        // 블루투스 기능 비활성화 시 블루투스 활성화
+        if (!blead.isEnabled()) {
             blead.enable();
         }
 
         // bluetooth 스캔 시작
-        ble.startScan();
+        blead.startLeScan(scancallback_le);
 
         Gson gson = new GsonBuilder().setLenient().create();
         retrofit = new Retrofit.Builder()
@@ -66,11 +75,9 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                data = ble.getOTP();
-
-                if(data != null) {
-                    Toast.makeText(getApplicationContext(), "페어링 기기 찾음", Toast.LENGTH_SHORT).show();
-                    ble.stopScan();
+                if(OTP_data != null) {
+                    Toast.makeText(getApplicationContext(), "Paired device found", Toast.LENGTH_SHORT).show();
+                    blead.stopLeScan(scancallback_le);
                 }
                 else handler.postDelayed(this, 500);
             }
@@ -81,26 +88,57 @@ public class MainActivity extends AppCompatActivity {
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(data != null) {
-                    Call<String> call = service.get(user, data);
+                if(OTP_data != null) {
+                    Call<String> call = service.post(user, OTP_data);
 
                     call.enqueue(new Callback<String>() {
                         @Override
                         public void onResponse(Call<String> call, Response<String> response) {
-                            Log.e("testGET", response.body().toString());
+                            Log.e("testSuccess", response.body().toString());
                         }
 
                         @Override
                         public void onFailure(Call<String> call, Throwable t) {
-                            Log.e("testGET", "fail to communicate with server", t);
+                            Log.e("testFail", "failed to communicate with server", t);
                         }
                     });
-
-                    Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "data가 비어있습니다.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "OTP_data is empty", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+    private BluetoothAdapter.LeScanCallback scancallback_le = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            String MacAdd = device.getAddress();
+
+            if(MacAdd.equals(raspberryPiAddr)) {
+                String data = byteArrayToHex(scanRecord);
+
+                OTP_data = extractOTP(data);
+            }
+        }
+    };
+
+    private String byteArrayToHex(byte[] bytes){
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b)); // 각 바이트를 16진수 문자열로 변환하여 추가
+        }
+        return sb.toString();
+    }
+
+    private String extractOTP(String hexData) {
+        String OTP = null;
+        String regExp = "998899([0-9a-fA-F]+)998899"; // OTP 추출 정규표현식
+
+        Pattern pattern = Pattern.compile(regExp);
+        Matcher matcher = pattern.matcher(hexData);
+
+        if (matcher.find()) {
+            OTP = matcher.group(1); // 매칭된 문자열을 추출하여 반환
+        }
+        return OTP;
     }
 }
