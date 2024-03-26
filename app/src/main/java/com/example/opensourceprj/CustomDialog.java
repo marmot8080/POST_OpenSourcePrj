b1e9eb6430ccacc9ba4fada6c3c01daca078cb72
marmot8080 <marmot8080@gmail.com>
2024-03-26 10:16:06 +0900

    update custom dialog

--- app/src/main/java/com/example/opensourceprj/CustomDialog.java
+++ app/src/main/java/com/example/opensourceprj/CustomDialog.java
@@ -9,19 +9,49 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 
 public class CustomDialog extends Dialog {
-    public TextView txt_content;
-    private Button btn_close;
+    private CustomDialogInterface customDialogInterface;
 
-    public CustomDialog(@NonNull Context context, String contents) {
+    public TextView txt_content;
+    private Button btn_cancel;
+    private Button btn_accept;
+
+    // setDialogListener()로 클릭 시 발생 이벤트 override 가능
+    interface CustomDialogInterface{
+        void cancelClicked();
+        void acceptClicked();
+    }
+
+    public void setDialogListener(CustomDialogInterface customDialogInterface){
+        this.customDialogInterface = customDialogInterface;
+    }
+
+    public CustomDialog(@NonNull Context context, String contents, String txt_btn_cancel, String txt_btn_accept) {
         super(context);
         setContentView(R.layout.activity_custom_dialog);
 
         txt_content = findViewById(R.id.Text_content);
         txt_content.setText(contents);
-        btn_close = findViewById(R.id.Btn_close);
-        btn_close.setOnClickListener(new View.OnClickListener() {
+
+        btn_cancel = findViewById(R.id.Dlg_btn_cancel);
+        btn_cancel.setText(txt_btn_cancel);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(customDialogInterface != null) {
+                    customDialogInterface.cancelClicked();
+                }
+                dismiss();
+            }
+        });
+
+        btn_accept = findViewById(R.id.Dlg_btn_accept);
+        btn_accept.setText(txt_btn_accept);
+        btn_accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(customDialogInterface != null) {
+                    customDialogInterface.acceptClicked();
+                }
                 dismiss();
             }
         });
