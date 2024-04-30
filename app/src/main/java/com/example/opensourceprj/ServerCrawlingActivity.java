dbf1a79e0b95e6be1078a0fd4b067974cb85c27b
marmot8080 <marmot8080@gmail.com>
2024-04-30 17:06:56 +0900

    updated layout

--- app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
+++ app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
@@ -1,6 +1,28 @@
 package com.example.opensourceprj;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ServerCrawlingActivity extends AppCompatActivity {
+    private Button btn_back;
+
+    private final String server_URL = "http://203.255.81.72:10021/dustsensor/sensingpage/"; // 서버 url
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server_crawling);
+
+        btn_back = findViewById(R.id.Btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
