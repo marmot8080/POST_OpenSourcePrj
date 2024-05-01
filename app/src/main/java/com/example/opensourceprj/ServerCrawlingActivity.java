b8e411ba550bde6f99f157f0778db8167527adc6
Taeho_Kim <teahotiger@gmail.com>
2024-05-01 18:22:31 +0900

    update layout

--- app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
+++ app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +13,10 @@ import java.io.IOException;
 
 public class ServerCrawlingActivity extends AppCompatActivity {
     private Button btn_back;
+    private Button btn_refresh;
 
+    private TextView show_data;
     private final String server_URL = "http://203.255.81.72:10021/dustsensor/sensingpage/"; // 서버 url
-
-    Handler handler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            Bundle bundle = msg.getData();	//new Thread에서 작업한 결과물 받기
-            textView.setText(bundle.getString("temperature"));	//받아온 데이터 textView에 출력
-        };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,28 +30,39 @@ public class ServerCrawlingActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        new Thread(){
+        btn_refresh = findViewById(R.id.Btn_refresh);
+        btn_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run(){
-                try{
-                  // start Crawling
-                    //Document doc = Jsoup.connect(server_URL).get();	//URL 웹사이트에 있는 html 코드 Crawling
+            public void onClick(View v) {
+                // 페이지 새로고침
+                @Override
+                public void run() {
+                    try {
+                        // start Crawling
+                        //Document doc = Jsoup.connect(server_URL).get();	//URL 웹사이트에 있는 html 코드 Crawling
 
-                    /*Elements sensDataNum = doc.select(".");	//끌어온 html에서 클래스네임이 "" 인 값만 선택해서 빼오기
-                    isEmpty = sensDataNum.isEmpty(); //빼온 값 null체크
-                    Log.d("Tag", "isNull? : " + isEmpty); //로그캣 출력
-                    if(isEmpty == false) { //null값이 아니면 크롤링 실행
-                        tem = temele.get(0).text().substring();
-                        bundle.putString("", tem); //bundle 이라는 자료형에 뽑아낸 결과값 담아서 main Thread로 보내기
-                    */
-                } catch (IOException e) {
+                /*Elements sensDataNum = doc.select(".");	//끌어온 html에서 클래스네임이 "" 인 값만 선택해서 빼오기
+                isEmpty = sensDataNum.isEmpty(); //빼온 값 null체크
+                Log.d("Tag", "isNull? : " + isEmpty); //로그캣 출력
+                if(isEmpty == false) { //null값이 아니면 크롤링 실행
+                    tem = temele.get(0).text().substring();
+                    bundle.putString("", tem); //bundle 이라는 자료형에 뽑아낸 결과값 담아서 main Thread로 보내기
+                */
+                    } catch (IOException e) {
 
-                    throw new RuntimeException(e);
+                        throw new RuntimeException(e);
+                    }
                 }
             }
-        }.start();
-
+        });
 
     }
+    /*
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Bundle bundle = msg.getData();	//new Thread에서 작업한 결과물 받기
+            show_data.setText(bundle.getString("temperature"));	//받아온 데이터 textView에 출력
+        };
+        */
 }
