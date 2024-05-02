c229788013c27ab6a85c4a5d4ca889b013785620
marmot8080 <marmot8080@gmail.com>
2024-05-02 13:34:30 +0900

    added crawling function

--- app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
+++ app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
@@ -1,19 +1,25 @@
 package com.example.opensourceprj;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
 public class ServerCrawlingActivity extends AppCompatActivity {
     private Button btn_back;
-    private Button btn_refresh;
+    private ImageButton btn_refresh;
 
     private TextView show_data;
     private final String server_URL = "http://203.255.81.72:10021/dustsensor/sensingpage/"; // 서버 url
@@ -31,37 +37,59 @@ public class ServerCrawlingActivity extends AppCompatActivity {
             }
         });
 
-        /*
         btn_refresh = findViewById(R.id.Btn_refresh);
         btn_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 페이지 새로고침
-
-                // start Crawling
-                Document doc = Jsoup.connect(server_URL).get();	//URL 웹사이트에 있는 html 코드 Crawling
-                Elements sensDataNum = doc.select(".");	//끌어온 html에서 클래스네임이 "" 인 값만 선택해서 빼오기
-                isEmpty = sensDataNum.isEmpty(); //빼온 값 null체크
-                Log.d("Tag", "isNull? : " + isEmpty); //로그캣 출력
-                if(isEmpty == false) { //null값이 아니면 크롤링 실행
-                    tem = temele.get(0).text().substring();
-                    bundle.putString("", tem); //bundle 이라는 자료형에 뽑아낸 결과값 담아서 main Thread로 보내기
-
-                    } catch (IOException e) {
-
-                        throw new RuntimeException(e);
-                    }
-
+                new NetworkTask().execute();
             }
-        });*/
+        });
 
+        new NetworkTask().execute();
     }
-    /*
-    Handler handler = new Handler(Looper.getMainLooper()) {
+
+    private class NetworkTask extends AsyncTask<Void, Void, Document> {
+
         @Override
-        public void handleMessage(@NonNull Message msg) {
-            Bundle bundle = msg.getData();	//new Thread에서 작업한 결과물 받기
-            show_data.setText(bundle.getString("temperature"));	//받아온 데이터 textView에 출력
-        };
-        */
+        protected Document doInBackground(Void... voids) {
+            try {
+                return Jsoup.connect(server_URL).get();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Document document) {
+            if (document != null) {
+                crawlData(document);
+            } else {
+                // 요청 실패 처리
+            }
+        }
+    }
+
+    public void crawlData(Document doc) {
+        Elements sensorData = doc.getElementsByTag("tr");
+
+        if (!sensorData.isEmpty()) {
+            show_data = findViewById(R.id.Text_sensing_data);
+            show_data.setText("");
+
+            for(Element row: sensorData) {
+                Elements rowDatas = row.select("th");
+
+                for(Element data: rowDatas) {
+                    show_data.append(data.text() + " ");
+                }
+
+                show_data.append("\n");
+            }
+
+            Log.d("Tag", "isNull? : " + "Non Null");
+        } else {
+            Log.d("Tag", "isNull? : " + "Null");
+        }
+    }
 }
