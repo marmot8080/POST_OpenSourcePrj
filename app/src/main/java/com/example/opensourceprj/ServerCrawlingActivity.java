ac0894e64fd674ac97c2f1bb6813057bfca06ee4
Taeho_Kim <teahotiger@gmail.com>
2024-05-08 14:56:36 +0900

    Added sensing web crawling feature extension

--- app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
+++ app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
@@ -1,19 +1,22 @@
 package com.example.opensourceprj;
 
-import android.os.Bundle;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
@@ -21,6 +24,7 @@ public class ServerCrawlingActivity extends AppCompatActivity {
     private Button btn_back;
     private ImageButton btn_refresh;
 
+    private WebView show_Web;
     private TextView show_data;
     private final String server_URL = "http://203.255.81.72:10021/dustsensor/sensingpage/"; // 서버 url
 
@@ -37,6 +41,7 @@ public class ServerCrawlingActivity extends AppCompatActivity {
             }
         });
 
+
         btn_refresh = findViewById(R.id.Btn_refresh);
         btn_refresh.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +51,37 @@ public class ServerCrawlingActivity extends AppCompatActivity {
         });
 
         new NetworkTask().execute();
+
+
+        WebView sensingWebView = (WebView) findViewById(R.id.Img_sensing_data);
+
+        sensingWebView.setWebViewClient(new WebViewClient()); // 새 창 띄우지 않기
+        sensingWebView.setWebChromeClient(new WebChromeClient());
+        /*
+        sensingWebView.setDownloadListener(new DownloadListener() { // 파일 다운로드 설정
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+
+            }
+        });
+        */
+
+        sensingWebView.getSettings().setLoadWithOverviewMode(true); // WebView 화면크기에 맞추도록 설정
+        sensingWebView.getSettings().setUseWideViewPort(true); // wide viewport 설정 - setLoadWithOverviewMode와 같이 사용
+
+        sensingWebView.getSettings().setSupportZoom(true); // 줌 설정 여부
+        sensingWebView.getSettings().setBuiltInZoomControls(false); // 줌 확대/축소 버튼 여부
+
+        sensingWebView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
+            // ==  webview.addJavascriptInterface(new AndroidBridge(), "android");
+        sensingWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascr ipt가 window.open()를 사용할 수 있도록 설정
+        sensingWebView.getSettings().setSupportZoom(true); // 멀티 윈도우 사용 여부
+
+        sensingWebView.getSettings().setDomStorageEnabled(true); // 로컬 스토리지(localStorage) 사용여부
+
+        // 웹페이지 호출 // webView.loadUrl(server_URL);
+        sensingWebView.loadUrl(server_URL);
+
     }
 
     private class NetworkTask extends AsyncTask<Void, Void, Document> {
