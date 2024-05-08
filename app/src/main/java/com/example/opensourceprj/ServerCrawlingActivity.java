456b17ae469c4618f4e38f45692c533ea32e4ab4
marmot8080 <marmot8080@gmail.com>
2024-05-08 15:15:37 +0900

    separated setWebViewPermissions()

--- app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
+++ app/src/main/java/com/example/opensourceprj/ServerCrawlingActivity.java
@@ -33,6 +33,8 @@ public class ServerCrawlingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_crawling);
 
+        new NetworkTask().execute();
+
         btn_back = findViewById(R.id.Btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +42,6 @@ public class ServerCrawlingActivity extends AppCompatActivity {
                 finish();
             }
         });
-
 
         btn_refresh = findViewById(R.id.Btn_refresh);
         btn_refresh.setOnClickListener(new View.OnClickListener() {
@@ -50,21 +51,9 @@ public class ServerCrawlingActivity extends AppCompatActivity {
             }
         });
 
-        new NetworkTask().execute();
-
-
         web_data = findViewById(R.id.Img_sensing_data);
 
-        web_data.setWebViewClient(new WebViewClient()); // 새 창 띄우지 않기
-        web_data.setWebChromeClient(new WebChromeClient());
-        web_data.getSettings().setLoadWithOverviewMode(true); // WebView 화면크기에 맞추도록 설정
-        web_data.getSettings().setUseWideViewPort(true); // wide viewport 설정 - setLoadWithOverviewMode와 같이 사용
-        web_data.getSettings().setSupportZoom(true); // 줌 설정 여부
-        web_data.getSettings().setBuiltInZoomControls(true); // 줌 확대/축소 버튼 여부
-        web_data.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
-        web_data.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascript가 window.open()를 사용할 수 있도록 설정
-        web_data.getSettings().setSupportZoom(true); // 멀티 윈도우 사용 여부
-        web_data.getSettings().setDomStorageEnabled(true); // 로컬 스토리지(localStorage) 사용여부
+        setWebViewPermissions(web_data);
 
         // 웹페이지 호출
         web_data.loadUrl(server_URL);
@@ -113,5 +102,18 @@ public class ServerCrawlingActivity extends AppCompatActivity {
         } else {
             Log.d("Tag", "isNull? : " + "Null");
         }
+    }
+
+    public void setWebViewPermissions(WebView webView) {
+        webView.setWebViewClient(new WebViewClient()); // 새 창 띄우지 않기
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setLoadWithOverviewMode(true); // WebView 화면크기에 맞추도록 설정
+        webView.getSettings().setUseWideViewPort(true); // wide viewport 설정 - setLoadWithOverviewMode와 같이 사용
+        webView.getSettings().setSupportZoom(true); // 줌 설정 여부
+        webView.getSettings().setBuiltInZoomControls(true); // 줌 확대/축소 버튼 여부
+        webView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascript가 window.open()를 사용할 수 있도록 설정
+        webView.getSettings().setSupportZoom(true); // 멀티 윈도우 사용 여부
+        webView.getSettings().setDomStorageEnabled(true); // 로컬 스토리지(localStorage) 사용여부
     }
 }
