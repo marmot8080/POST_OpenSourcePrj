12f1c0b202661f15bf23b5881ac9d768e523647e
marmot8080 <marmot8080@gmail.com>
2024-05-09 13:59:32 +0900

    added ConnectedThread

---
+++ app/src/main/java/com/example/opensourceprj/ConnectedThread.java
@@ -0,0 +1,74 @@
+package com.example.opensourceprj;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.SystemClock;
+import android.system.SystemCleaner;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class ConnectedThread extends Thread{
+    private final BluetoothSocket mmSocket;
+    private final InputStream mmInStream;
+    private final OutputStream mmOutStream;
+
+    public ConnectedThread(BluetoothSocket socket) {
+        mmSocket = socket;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        try {
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mmInStream = tmpIn;
+        mmOutStream = tmpOut;
+    }
+
+    @Override
+    public void run() {
+        byte[] buffer = new byte[1024];
+        int bytes;
+
+        while (true) {
+            try {
+                bytes = mmInStream.available();
+
+                if(bytes != 0) {
+                    buffer = new byte[1024];
+                    SystemClock.sleep(100);
+                    bytes = mmInStream.available();
+                    bytes = mmInStream.read(buffer, 0, bytes);
+                    String data = new String(buffer, StandardCharsets.UTF_8);
+                    Log.e("data", data);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public void write(String input) {
+        byte[] bytes = input.getBytes();
+        try {
+            mmOutStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
