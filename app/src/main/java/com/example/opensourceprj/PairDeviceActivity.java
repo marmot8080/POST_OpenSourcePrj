fb1f985772dda782a8019a16594fa1fa7cc443c8
marmot8080 <marmot8080@gmail.com>
2024-05-09 12:07:38 +0900

    added PairDeviceActivity

---
+++ app/src/main/java/com/example/opensourceprj/PairDeviceActivity.java
@@ -0,0 +1,14 @@
+package com.example.opensourceprj;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PairDeviceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pair_device);
+    }
+}
