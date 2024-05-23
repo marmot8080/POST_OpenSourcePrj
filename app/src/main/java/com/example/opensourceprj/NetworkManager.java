77cbec930a88acf0f47470adefe4a868a7f11481
marmot8080 <marmot8080@gmail.com>
2024-05-23 22:00:20 +0900

    added wifi scan function

--- app/src/main/java/com/example/opensourceprj/NetworkManager.java
+++ app/src/main/java/com/example/opensourceprj/NetworkManager.java
@@ -2,6 +2,10 @@ package com.example.opensourceprj;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
 
 public class NetworkManager {
     public static final int WIFI_CONNECTED = 1;     // 와이파이 연결 상태
@@ -9,9 +13,9 @@ public class NetworkManager {
     public static final int NOT_CONNECTED = 3; // 미연결 상태
     
     public static int getConnectivityStatus(Context context) {
-        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         //networkInfo 값이 null일 수도 있기 때문에
         if (networkInfo != null) {
             int type = networkInfo.getType();
@@ -22,5 +26,13 @@ public class NetworkManager {
                 return WIFI_CONNECTED;
             }
         } return NOT_CONNECTED;
+    }
+
+    public List<ScanResult> getScanResults(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        wifiManager.startScan();
+
+        return wifiManager.getScanResults();
     }
 }
